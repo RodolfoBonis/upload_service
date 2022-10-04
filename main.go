c187b7f5aa858bf2dcc60db884f83e2924779480
RodolfoDBonis <dev@rodolfodebonis.com.br>
2022-10-04 18:48:54 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, &echo.Map{"data": "Hello from Cloudinary"})
+	})
+
+	e.Logger.Fatal(e.Start(":6000"))
+
+}
