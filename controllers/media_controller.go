92d0ed9108fa67aa23a0ef01bef95758ad2b5634
RodolfoDBonis <dev@rodolfodebonis.com.br>
2022-10-06 00:40:19 -0300

    finished upload media with local file or remote URL

---
+++ controllers/media_controller.go
@@ -0,0 +1,99 @@
+package controllers
+
+import (
+	"github.com/RodolfoBonis/upload_service/dtos"
+	"github.com/RodolfoBonis/upload_service/models"
+	"github.com/RodolfoBonis/upload_service/services"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+var mediaService = services.NewMediaUpload()
+
+type mediaController interface {
+	FileUpload(c echo.Context) error
+	RemoteUpload(c echo.Context) error
+}
+
+type media struct{}
+
+func NewMediaController() mediaController {
+	return &media{}
+}
+
+func (*media) FileUpload(c echo.Context) error {
+	formHeader, err := c.FormFile("file")
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError,
+			dtos.MediaDto{
+				StatusCode: http.StatusInternalServerError,
+				Message:    "Error",
+				Data:       &echo.Map{"data": "Select a file to upload"},
+			})
+	}
+
+	formFile, err := formHeader.Open()
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError,
+			dtos.MediaDto{
+				StatusCode: http.StatusInternalServerError,
+				Message:    "error",
+				Data:       &echo.Map{"data": err.Error()},
+			})
+	}
+
+	uploadUrl, err := mediaService.FileUpload(models.FileModel{File: formFile})
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError,
+			dtos.MediaDto{
+				StatusCode: http.StatusInternalServerError,
+				Message:    "error",
+				Data:       &echo.Map{"data": err.Error()},
+			})
+	}
+
+	return c.JSON(http.StatusOK,
+		dtos.MediaDto{
+			StatusCode: http.StatusOK,
+			Message:    "success",
+			Data:       &echo.Map{"data": uploadUrl},
+		})
+}
+
+func (*media) RemoteUpload(c echo.Context) error {
+	var url models.UrlModel
+
+	if err := c.Bind(&url); err != nil {
+		return c.JSON(
+			http.StatusBadRequest,
+			dtos.MediaDto{
+				StatusCode: http.StatusBadRequest,
+				Message:    "error",
+				Data:       &echo.Map{"data": err.Error()},
+			})
+	}
+
+	uploadUrl, err := mediaService.RemoteUpload(url)
+
+	if err != nil {
+		return c.JSON(
+			http.StatusInternalServerError,
+			dtos.MediaDto{
+				StatusCode: http.StatusInternalServerError,
+				Message:    "error",
+				Data:       &echo.Map{"data": "Error uploading file"},
+			})
+	}
+
+	return c.JSON(
+		http.StatusOK,
+		dtos.MediaDto{
+			StatusCode: http.StatusOK,
+			Message:    "success",
+			Data:       &echo.Map{"data": uploadUrl},
+		})
+
+}
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"github.com/RodolfoBonis/upload_service/controllers"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
+
+var mediaController = controllers.NewMediaController()
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, &echo.Map{"data": "Hello from Cloudinary"})
+	e.POST("/upload", func(c echo.Context) error {
+		_, err := c.FormFile("file")
+
+		if err != nil {
+			return mediaController.RemoteUpload(c)
+		} else {
+			return mediaController.FileUpload(c)
+		}
 	})
 
 	e.Logger.Fatal(e.Start(":6000"))
